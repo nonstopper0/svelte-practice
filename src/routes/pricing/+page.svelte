ff458bade35ddc066ace472b45961e5662d105c8
Nathaniel Redmon <nathanielredmon@gmail.com>
2023-07-18 23:37:21 -0400

    test

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import PricingCard from './PricingCard.svelte';
+
+	let tiers = [
+		{
+			name: 'Basic',
+			price: '10',
+			features: ['Feature 1', 'Feature 2', 'Feature 3'],
+			icon: '/path/to/icon1.png'
+		},
+		{
+			name: 'Pro',
+			price: '20',
+			features: ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4'],
+			icon: '/path/to/icon2.png'
+		},
+		{
+			name: 'Ultimate',
+			price: '30',
+			features: ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4'],
+			icon: '/path/to/icon3.png'
+		}
+	];
+</script>
+
+<div class="pricing-container">
+	{#each tiers as tier (tier.name)}
+		<PricingCard {tier} />
+	{/each}
+</div>
+
+<style>
+	.pricing-container {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 20px;
+		padding: 20px;
+		background: linear-gradient(to right, #ff5f6d, #ffc371);
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/pricing/PricingCard.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let tier;
+</script>
+
+<div class="card" on:mouseover={hover} on:mouseout={unhover}>
+	<img src={tier.icon} alt={tier.name} />
+	<h2>{tier.name}</h2>
+	<h1>${tier.price}</h1>
+	<ul>
+		{#each tier.features as feature (feature)}
+			<li>{feature}</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.card {
+		padding: 20px;
+		background: rgba(255, 255, 255, 0.1);
+		backdrop-filter: blur(10px);
+		border-radius: 15px;
+		color: #fff;
+		text-align: center;
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		transition: transform 0.5s;
+	}
+	.card:hover {
+		transform: scale(1.05);
+	}
+	.card img {
+		width: 50px;
+		height: 50px;
+	}
+	.card h2 {
+		font-family: 'Poppins', sans-serif;
+		font-size: 24px;
+		margin: 20px 0;
+	}
+	.card h1 {
+		font-family: 'Poppins', sans-serif;
+		font-size: 48px;
+		margin: 20px 0;
+	}
+	.card ul {
+		list-style-type: none;
+		padding: 0;
+		font-family: 'Poppins', sans-serif;
+	}
+</style>
